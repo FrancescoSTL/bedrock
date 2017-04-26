4e0c670b0dae61a03c6db0102db8975bbebfd7a6
FrancescoSTL <francescosapolizzi@gmail.com>
2017-04-25 22:24:17 -0500

    Refactor CSS+templates

--- bedrock/settings/static_media.py
+++ bedrock/settings/static_media.py
@@ -526,26 +526,28 @@ PIPELINE_CSS = {
         ),
         'output_filename': 'css/firefox_new_way_of_the_fox-bundle.css',
     },
-    'firefox_new_private_not_option': {
+    'firefox_new_lifestyle_base': {
         'source_filenames': (
             'css/newsletter/fxnewsletter-subscribe.less',
             'css/firefox/family-nav.less',
+            'css/firefox/new/fx-lifestyle-base.less',
+        ),
+        'output_filename': 'css/firefox_new_lifestyle_base.css',
+    },
+    'firefox_new_private_not_option': {
+        'source_filenames': (
             'css/firefox/new/private-not-option.less',
         ),
         'output_filename': 'css/firefox_new_private-not-option-bundle.css',
     },
     'firefox_new_conformity_not_default': {
         'source_filenames': (
-            'css/newsletter/fxnewsletter-subscribe.less',
-            'css/firefox/family-nav.less',
             'css/firefox/new/conformity-not-default.less',
         ),
         'output_filename': 'css/firefox_new_private-not-option-bundle.css',
     },
     'firefox_new_browse_up_to_you': {
         'source_filenames': (
-            'css/newsletter/fxnewsletter-subscribe.less',
-            'css/firefox/family-nav.less',
             'css/firefox/new/browse-up-to-you.less',
         ),
         'output_filename': 'css/firefox_new_browse-up-to-you.css',
